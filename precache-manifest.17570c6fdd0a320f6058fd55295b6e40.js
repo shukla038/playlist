514d51e21602e8d4600ed02f3872638af06261d1
Aman <servify@Sl-sp-lap-0331.local>
2019-10-13 20:54:25 +0530

    Updates

---
+++ precache-manifest.17570c6fdd0a320f6058fd55295b6e40.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2fc4a3ad3fb93e3b44cd8121d0f012",
+    "url": "/playlist/index.html"
+  },
+  {
+    "revision": "3ecba5a63b94c92672d7",
+    "url": "/playlist/static/css/4.8701e8f8.chunk.css"
+  },
+  {
+    "revision": "a3bc70f2a33165b2443c",
+    "url": "/playlist/static/css/main.45ee8331.chunk.css"
+  },
+  {
+    "revision": "5aeb2f473a0d42078df2",
+    "url": "/playlist/static/js/2.5fdeda63.chunk.js"
+  },
+  {
+    "revision": "2230ec01c999380f15bb",
+    "url": "/playlist/static/js/3.2f940b2c.chunk.js"
+  },
+  {
+    "revision": "3ecba5a63b94c92672d7",
+    "url": "/playlist/static/js/4.537dd9a6.chunk.js"
+  },
+  {
+    "revision": "a3bc70f2a33165b2443c",
+    "url": "/playlist/static/js/main.edad5414.chunk.js"
+  },
+  {
+    "revision": "4fec2fa6cfb0bbb4c412",
+    "url": "/playlist/static/js/runtime~main.fb587f06.js"
+  }
+]);
